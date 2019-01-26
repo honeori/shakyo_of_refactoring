371a7ca164aa5377444b6087db28f857f9cf619f
honeori <ststlange@gmail.com>
2019-01-26 15:36:39 +0000

    shakyo first example by rust

--- chapter1/example/src/main.rs
+++ chapter1/example/src/main.rs
@@ -8,17 +8,14 @@ use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
 use std::collections::HashMap;
+use std::cmp::max;
 
 
 fn main() {
     let plays = plays();
     let invoice = invoice();
-    println!("hamlet names is {}", plays["hamlet"].name);
-    println!("the first invoice is {}", invoice.customer);
-
-//    let invoices = invoice();
-//    let invoice = &invoices[0];
-    //statement(invoice, &plays);
+    let result = statement(&invoice, &plays);
+    println!("{}", result);
 }
 
 #[derive(Serialize, Deserialize)]
@@ -39,18 +36,52 @@ struct Invoice {
     performances: Vec<Performance>,
 }
 
-fn statement(invoice: &Value, plays: &Value) {
-    println!("hamlet names is {}", plays["hamlet"]["name"].as_str().unwrap());
-    println!("the first invoice is {}", invoice["customer"].as_str().unwrap());
+fn statement(invoice: &Invoice, plays: &HashMap<String, Play>) -> String {
     let mut total_amount = 0;
     let mut volume_credits = 0;
-    let result = format!("Statement for {}\n", invoice["customer"].as_str().unwrap());
+    let mut result = format!("Statement for {}\n", invoice.customer);
 
-    for perf in invoice["performances"].as_array().unwrap() {
-        let playID = perf["playID"].as_str().unwrap();
-        let play = &plays[playID];
-        let thisAmount = 0;
+    for perf in &invoice.performances {
+        let play = plays.get(&perf.playID).unwrap();
+        let mut this_amount = 0;
+        match play.play_type.as_ref() {
+            "tragedy" => {
+                this_amount = 40000;
+                if perf.audience > 30 {
+                    let temp = perf.audience as u32;
+                    this_amount += 1000 * (temp - 30);
+                }
+            },
+            "comedy" => {
+                this_amount = 30000;
+                let temp = perf.audience as u32;
+                if perf.audience > 20 {
+                    this_amount += 10000 + 500 * (temp - 20);
+                }
+                this_amount += 300 * temp;
+            },
+            _ => panic!("unknown play_type: {}", play.play_type)
+        };
+        // add volume credits
+        volume_credits += max(perf.audience - 30, 0);
+        // add extra credit for every ten comedy attendees
+        match play.play_type.as_ref() {
+            "comedy" => {
+                let temp = perf.audience as f64;
+                volume_credits += (temp / 5.0).floor() as u8;
+            },
+            _ => (),
+        }
+
+        result.push_str(format!("{}: {} ({} seats)\n", play.name, this_amount / 100, perf.audience).as_ref());
+        // print line for this order
+
     }
+
+    result.push_str(format!("Amount owed is {}\n", total_amount / 100).as_ref());
+    result.push_str(format!("You earned {} credits\n", volume_credits).as_ref());
+
+    result
 }
 
 fn plays() -> HashMap<String, Play> {
