7f97f8c14e868770485b49f2904fc74f853790d0
honeori <ststlange@gmail.com>
2019-01-26 16:11:58 +0000

    add a simple test

--- chapter1/example/src/lib.rs
+++ chapter1/example/src/lib.rs
@@ -1,6 +1,7 @@
 
 #[macro_use]
 extern crate serde_derive;
+extern crate serde_json;
 
 extern crate serde;
 
@@ -77,12 +78,59 @@ pub fn statement(invoice: &Invoice, plays: &HashMap<String, Play>) -> String {
 }
 
 
+fn plays() -> HashMap<String, Play> {
+    let data = r#"
+{
+    "hamlet": {"name": "Hamlet", "play_type": "tragedy"},
+    "as-like": {"name": "As You Like It", "play_type": "comedy"},
+    "othello": {"name": "Othello", "play_type": "tragedy"}
+}
+    "#;
+    let v: HashMap<String, Play> = serde_json::from_str(data).unwrap();
+
+    v
+}
+
+fn invoice() -> Invoice {
+    let data = r#"
+    {
+        "customer": "BigCo",
+        "performances": [
+            {
+                "playID": "hamlet",
+                "audience": 55
+            },
+            {
+                "playID": "as-like",
+                "audience": 35
+            },
+            {
+                "playID": "othello",
+                "audience": 40
+            }
+        ]
+    }
+    "#;
+    let v: Invoice = serde_json::from_str(data).unwrap();
+    v
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
-    fn sample() {
-        assert_eq!(1, 1)
+    fn first_statement() {
+        let plays = plays();
+        let invoice = invoice();
+        let result = statement(&invoice, &plays);
+        assert_eq!(result, r#"Statement for BigCo
+Hamlet: 650 (55 seats)
+As You Like It: 580 (35 seats)
+Othello: 500 (40 seats)
+Amount owed is 0
+You earned 47 credits
+"#
+        );
     }
 }
